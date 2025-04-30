f7c8442f3e633e85311f30b40e560b32e5ceb28f
ypb-sd12 <takahiro.hado@yoshida-p.co.jp>
2025-04-30 11:32:49 +0900

    js buttonを修正

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,8 +1,10 @@
 const button = document.getElementById("btn");
 const delay = 2000;
-button.addEventListener("click", setTimeout(() => {
-    const message = document.getElementById("text");
-    message.textContent = "ボタンをクリックしました"
-}, delay));
+button.addEventListener("click", () => {
+    setTimeout(() => {
+        const message = document.getElementById("text");
+        message.textContent = "ボタンをクリックしました"
+    }, delay);
+});
 
 
